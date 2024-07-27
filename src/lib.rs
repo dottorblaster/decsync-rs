71f3c3033f37261746e1a24de070bbc30152b58e
Alessio Biancalana <alessio@dottorblaster.it>
2024-07-27 23:17:06 +0200

    Add constructor

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+pub struct DecSyncInstance {
+    pub decsync_dir: String,
+    pub sync_type: String,
+    pub collection: String,
+    pub own_app_id: String,
+}
+
+impl DecSyncInstance {
+    pub fn new(
+        decsync_dir: String,
+        sync_type: String,
+        collection: String,
+        own_app_id: String,
+    ) -> DecSyncInstance {
+        DecSyncInstance {
+            decsync_dir,
+            sync_type,
+            collection,
+            own_app_id,
+        }
+    }
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -10,5 +33,19 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn decsync_is_constructed() {
+        let decsync_instance = DecSyncInstance::new(
+            "directory".to_owned(),
+            "all".to_owned(),
+            "all".to_owned(),
+            "org.gnome.Newsflash".to_owned(),
+        );
+        assert_eq!(
+            decsync_instance.own_app_id,
+            "org.gnome.Newsflash".to_owned()
+        )
     }
 }
